3b3fd0d99f8b326a63ae4bce7a90bdda35804d77
Rohan Chidurala <59586662+rohan-chidurala@users.noreply.github.com>
2025-05-16 22:07:49 +0000

    basic dungeon code

---
+++ dungeon.c
@@ -0,0 +1,250 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+#include <time.h>
+#include <stdbool.h>
+
+#define GRID_SIZE 4
+#define NUM_ROOMS 10
+#define NUM_DUNGEONS 4 // Number of dungeons
+
+// Room structure definition
+typedef struct
+{
+    int id;
+    int x;
+    int y;
+    char description[256];
+    bool isItemRoom;
+    bool isConnectorRoom;
+    bool isStartRoom;
+    int connectedDungeon; // Which dungeon this connector leads to (-1 if none)
+} Room;
+
+// First dungeon
+Room dungeon1[10] = {
+    {0, 0, 3, "You are at coordinates (0,3)", false, false, true, -1}, // Start Room (top left)
+    {1, 1, 3, "You are at coordinates (1,3)", false, false, false, -1},
+    {2, 2, 3, "You are at coordinates (2,3)", false, false, false, -1},
+    {3, 0, 2, "You are at coordinates (0,2)", false, false, false, -1},
+    {4, 3, 0, "You are at coordinates (3,0)", false, true, false, 1}, // Connector Room (bottom right)
+    {5, 2, 2, "You are at coordinates (2,2)", false, false, false, -1},
+    {6, 0, 1, "You are at coordinates (0,1)", false, false, false, -1},
+    {7, 1, 1, "You are at coordinates (1,1)", false, false, false, -1},
+    {8, 2, 1, "You are at coordinates (2,1)", true, false, false, -1}, // Item Room
+    {9, 3, 1, "You are at coordinates (3,1)", false, false, false, -1}};
+
+// Second dungeon
+Room dungeon2[10] = {
+    {0, 0, 3, "You are at coordinates (0,3)", false, true, true, 0}, // Start Room (top left) - also connects to main dungeon
+    {1, 1, 3, "You are at coordinates (1,3)", false, false, false, -1},
+    {2, 2, 3, "You are at coordinates (2,3)", false, false, false, -1},
+    {3, 0, 2, "You are at coordinates (0,2)", false, false, false, -1},
+    {4, 3, 0, "You are at coordinates (3,0)", false, true, false, 0}, // Connector Room (bottom right)
+    {5, 2, 2, "You are at coordinates (2,2)", false, false, false, -1},
+    {6, 0, 1, "You are at coordinates (0,1)", false, false, false, -1},
+    {7, 1, 1, "You are at coordinates (1,1)", false, false, false, -1},
+    {8, 2, 1, "You are at coordinates (2,1)", true, false, false, -1}, // Item Room
+    {9, 3, 1, "You are at coordinates (3,1)", false, false, false, -1}};
+
+// Array of dungeon pointers for easy access
+Room *dungeons[NUM_DUNGEONS] = {dungeon1, dungeon2, NULL, NULL};
+
+
+int currentRoom = 0;
+int currentDungeon = 0;
+
+// Find room by coordinates
+int findRoomByCoords(int x, int y)
+{
+    for (int i = 0; i < NUM_ROOMS; i++)
+    {
+        if (dungeons[currentDungeon][i].x == x && dungeons[currentDungeon][i].y == y)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+void printRoomDescription(int roomId)
+{
+    printf("\n%s\n", dungeons[currentDungeon][roomId].description);
+    printf("Current Dungeon: %d\n", currentDungeon);
+
+    if (dungeons[currentDungeon][roomId].isConnectorRoom &&
+        dungeons[currentDungeon][roomId].connectedDungeon != -1)
+    {
+        printf("This connector leads to Dungeon %d\n",
+               dungeons[currentDungeon][roomId].connectedDungeon);
+    }
+
+}
+
+int movePlayer(char direction)
+{
+    int newX = dungeons[currentDungeon][currentRoom].x;
+    int newY = dungeons[currentDungeon][currentRoom].y;
+
+    switch (direction)
+    {
+    case 'N':
+    case 'n':
+        newY++;
+        break;
+    case 'S':
+    case 's':
+        newY--;
+        break;
+    case 'E':
+    case 'e':
+        newX++;
+        break;
+    case 'W':
+    case 'w':
+        newX--;
+        break;
+    default:
+        printf("Invalid direction! Use N, S, E, or W.\n");
+        return 0;
+    }
+
+    // Check if we're trying to move through a connector
+    if ((dungeons[currentDungeon][currentRoom].isConnectorRoom ||
+         dungeons[currentDungeon][currentRoom].isStartRoom) &&
+        dungeons[currentDungeon][currentRoom].connectedDungeon != -1)
+    {
+        // If moving in the direction that would take us off the grid
+        if ((direction == 'N' && newY >= GRID_SIZE) ||
+            (direction == 'S' && newY < 0) ||
+            (direction == 'E' && newX >= GRID_SIZE) ||
+            (direction == 'W' && newX < 0))
+        {
+
+            int nextDungeon = dungeons[currentDungeon][currentRoom].connectedDungeon;
+            printf("Moving to Dungeon %d...\n", nextDungeon);
+            currentDungeon = nextDungeon;
+
+            // If it's a start room, go to the connector room instead of room 0
+            if (dungeons[currentDungeon][currentRoom].isStartRoom)
+            {
+                int connectorRoom = -1;
+                for (int i = 0; i < NUM_ROOMS; i++)
+                {
+                    if (dungeons[currentDungeon][i].isConnectorRoom)
+                    {
+                        connectorRoom = i;
+                        break;
+                    }
+                }
+                currentRoom = connectorRoom;
+            }
+            // Go to starting room
+            else
+            {
+                currentRoom = 0; // Start at the start room of the new dungeon
+            }
+
+            printf("Entered Dungeon %d\n", currentDungeon);
+            printRoomDescription(currentRoom);
+            return 1;
+        }
+    }
+
+    int newRoom = findRoomByCoords(newX, newY);
+    if (newRoom != -1)
+    {
+        currentRoom = newRoom;
+        printRoomDescription(currentRoom);
+        return 1;
+    }
+
+    printf("Cannot move in that direction - no room there!\n");
+    return 0;
+}
+
+void printDungeon()
+{
+    printf("\nDungeon Layout (Dungeon %d):\n", currentDungeon);
+    printf("   0   1   2   3\n");
+    printf("  +---+---+---+---+\n");
+
+    for (int y = 3; y >= 0; y--)
+    {
+        printf("%d |", y);
+        for (int x = 0; x < 4; x++)
+        {
+            int room = findRoomByCoords(x, y);
+            if (room != -1)
+            {
+                if (room == currentRoom)
+                {
+                    printf(" * |"); // Current room
+                }
+                else if (dungeons[currentDungeon][room].isStartRoom)
+                {
+                    printf(" S |"); // Start room
+                }
+                else if (dungeons[currentDungeon][room].isConnectorRoom)
+                {
+                    printf(" C |"); // Connector room
+                }
+                else if (dungeons[currentDungeon][room].isItemRoom)
+                {
+                    printf(" I |"); // Item room
+                }
+                else
+                {
+                    printf(" R |"); // Regular room
+                }
+            }
+            else
+            {
+                printf("   |"); // Empty space
+            }
+        }
+        printf("\n  +---+---+---+---+\n");
+    }
+}
+
+int main()
+{
+    char input[10];
+
+    // Initialize random seed
+    srand(time(NULL));
+
+    // Start in a random room
+    currentRoom = rand() % 10;
+
+    printf("Welcome to the Dungeon!\n");
+    printf("Type 'H' for help, 'Q' to quit.\n");
+    printRoomDescription(currentRoom);
+
+    while (1)
+    {
+        printf("\nEnter command: ");
+        if (fgets(input, sizeof(input), stdin) == NULL)
+            break;
+
+        // Remove newline
+        input[strcspn(input, "\n")] = 0;
+
+        if (input[0] == 'D' || input[0] == 'd')
+        {
+            printDungeon();
+        }
+        else if (strlen(input) == 1)
+        {
+            movePlayer(input[0]);
+        }
+        else
+        {
+            printf("Invalid command!");
+        }
+    }
+
+    return 0;
+}
